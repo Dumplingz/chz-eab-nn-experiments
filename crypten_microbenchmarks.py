1fe2d533b80f6e85017e424d24b3c426c91e859f
Chris Zhu <chz@uchicago.edu>
2024-05-14 19:56:23 +0000

    more micro experiments

--- crypten_microbenchmarks.py
+++ crypten_microbenchmarks.py
@@ -16,31 +16,82 @@ def main():
     torch.set_num_threads(1)
 
     loss_fn = crypten.nn.CrossEntropyLoss()
+    plain_loss_fn = nn.CrossEntropyLoss()
 
 
     # Create torch tensor
-    x = torch.tensor([1.2e7, 2e9, -3e10])
-    y = torch.tensor([1.0,0,0])
+    x = torch.tensor([[0.01,0.02,0.001],[4,5,6]])
+    y = torch.tensor([[1.0,0,0],[0,0,1]])
 
     # Encrypt x,y
     x_enc = crypten.cryptensor(x)
     y_enc = crypten.cryptensor(y)
 
-    loss = loss_fn(x_enc, y_enc)
+    x_softmax_enc = x_enc.softmax(dim=1)
+    print("softmax enc", x_softmax_enc.get_plain_text())
+    print("softmax plain", x.softmax(dim=1))
 
+    dec_loss = plain_loss_fn(x,y)
+    print("CrossEntropy Plaintext Loss" , dec_loss)
+
+
+    loss = loss_fn(x_enc, y_enc)
 
     # Decrypt x
     x_dec = x_enc.get_plain_text()
-    crypten.print(x_dec)
+    crypten.print("decrypted x", x_dec)
 
     loss_dec = loss.get_plain_text()
-    crypten.print("Crypten Loss", loss_dec)
+    crypten.print("CrossEntropy Crypten Loss", loss_dec)
 
-    dec_loss_fn = nn.CrossEntropyLoss()
+    # random loss tests
+    rand = torch.randn(3,5) * 100
+    print("base rand", rand)
+    target = torch.randn(3, 5).softmax(dim=1)
+    print("target", target)
+    plain_loss = plain_loss_fn(rand, target)
 
-    dec_loss = dec_loss_fn(x,y)
 
-    print("Plaintext Loss" , dec_loss)
+
+    # crypten random loss
+    rand_enc = crypten.cryptensor(rand)
+    target_enc = crypten.cryptensor(target)
+
+    print("softmax plain", rand.softmax(dim=1))
+    print("softmax enc", rand_enc.softmax(dim=1).get_plain_text())
+
+    loss = loss_fn(rand_enc, target_enc)
+
+    dec_loss = loss.get_plain_text()
+
+    print("plaintext loss", plain_loss)
+
+    crypten.print("crypten loss", dec_loss)
+
+    rand_enc_softmax = rand_enc.softmax(dim=1)
+
+    loss_values = rand_enc_softmax.log(input_in_01=True).mul_(target_enc).neg_()
+    crypten.print("log, mul, neg:", loss_values.get_plain_text())
+    final_values = loss_values.sum().div_(target_enc.size(0))
+    crypten.print("sum, div:", final_values.get_plain_text())
+
+
+
+    zeros_test = torch.zeros(3,5)
+    zeros_log_test = zeros_test.log()
+    print("zeros test", zeros_log_test)
+
+    zeros_test_enc = crypten.cryptensor(torch.zeros(3,5))
+    zeros_log_test_enc = zeros_test_enc.log(input_in_01=True)
+    print("zeros test", zeros_log_test_enc.get_plain_text())
+
+
+    large_x = torch.tensor([100000200302512352])
+    print(large_x)
+
+    large_x_enc = crypten.cryptensor(large_x)
+
+    print(large_x_enc.get_plain_text())
 
 if __name__ == "__main__":
     main()
